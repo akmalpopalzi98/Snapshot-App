63d465850b83eeb37665ef1d6047a4d8eceed9c3
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-10-25 13:00:28 +0100

    chore: refactoring component

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import { Box } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
+import { useSearchBar } from "../hooks/useSearchBar";
 
 const SearchBar = () => {
+  const { text, changeText } = useSearchBar();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    window.history.pushState({}, "", `/${text}`);
+  };
+  console.log(text);
+
   return (
-    <Box
-      sx={{ display: "flex", justifyContent: "center", marginBottom: "30px" }}
-    >
-      <input style={{ fontSize: "16px", padding: "10px", height: "10px" }} />
-      <button
-        style={{
-          backgroundColor: " #212f3d",
-          color: "white",
-          border: "none",
+    <form onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "30px",
+          alignItems: "center",
         }}
       >
-        <SearchIcon />
-      </button>
-    </Box>
+        <input
+          style={{ fontSize: "16px", padding: "10px", height: "10px" }}
+          placeholder="search image"
+          onChange={(event) => {
+            changeText(event.target.value);
+          }}
+        />
+        <button
+          style={{
+            backgroundColor: isHovered ? "#1abc9c" : "#212f3d",
+            color: "white",
+            border: "none",
+            marginLeft: "5px",
+            cursor: "pointer",
+            transition: "background-color 0.3s",
+          }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <SearchIcon />
+        </button>
+      </Box>
+    </form>
   );
 };
 
