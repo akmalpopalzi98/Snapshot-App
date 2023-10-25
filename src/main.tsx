76c607fc360e37faf88b419832c15e0ff1f317f6
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-10-25 12:59:02 +0100

    chore: adding searchbar provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { SearchProvider } from "./context/searchBarContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
