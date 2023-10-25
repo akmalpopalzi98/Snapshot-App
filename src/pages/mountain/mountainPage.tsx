32f067c99654b472f00653e5add2b6eb8786a89b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-10-25 21:52:33 +0100

    chore: implementing api

--- src/pages/mountain/mountainPage.tsx
+++ src/pages/mountain/mountainPage.tsx
@@ -1,5 +1,25 @@
-const mountainPage = () => {
-  return <div>Mountain Page</div>;
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { fetchData } from "../../API";
+
+const MountainPage = () => {
+  const [imageList, setImageList] = useState([{}]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetchData("mountains");
+        setImageList(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  console.log(imageList);
+
+  return <Box>Mountain Page</Box>;
 };
 
-export default mountainPage;
+export default MountainPage;
